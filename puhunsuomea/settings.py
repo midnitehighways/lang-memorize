b36bd37db38704711689750ae97f5f8251f97f79
Alex <alexandru.oat@gmail.com>
2016-02-21 19:08:01 +0200

    Change DB to PostgreSQL

--- puhunsuomea/settings.py
+++ puhunsuomea/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -75,12 +76,26 @@ WSGI_APPLICATION = 'puhunsuomea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME'),  #get_env_variable('DATABASE_NAME'),
+        'USER': os.environ.get('USER_NAME'),   #get_env_variable('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),      #get_env_variable('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Internationalization
